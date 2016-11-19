04042ef08d2dd069f9c2a0da40ad20abcd36bbe5
borsosfi <borsosfi@gmail.com>
2016-11-19 14:26:37 +0100

    Remove unused codes, change variables

--- src/js/directives/matchRow.js
+++ src/js/directives/matchRow.js
@@ -1,17 +1,19 @@
-ihr.directive("matchRow", ["$http", "Config", "matchFactory", function($http, Config, matchFactory){
+ihr.directive("matchRow", ["$http", "playerService", function($http, playerService){
     return {
         restrict: 'AE',
         templateUrl: "templates/directives/matchRow.html",
         scope: {
-            matches: "=",
             serverError: "="
         },
-        controller: ['$scope', "$http", "Config", function($scope, $http, Config){
+        controller: ['$scope', "$http", function($scope, $http){
+            this.players = [];
+          
+            $scope.getPlayerList = function() {
+              this.players = playerService.getPlayerList();
+            };
             
-            var dataUrl = Config.apiServer + "/matches";
-            
-            $scope.updateRow = function(row){
-                matchFactory.updateMatch(row).then(function(response){
+            $scope.updatePlayer = function(player) {
+                playerService.updatePlayer(player).then(function(response){
                             console.log(response);
                             $scope.serverError = false;
                         }, function(err){
@@ -19,17 +21,7 @@ ihr.directive("matchRow", ["$http", "Config", "matchFactory", function($http, Co
                             console.error(err);
                         }
                 );
-               
-            };
-            
-            $scope.deleteRow = function(match){
-                if (!confirm("Are you sure you want to delete the " + match.home + "-" + match.away + " match?")){
-                    return;
-                }
-                
-                var index = $scope.matches.indexOf(match);
-                $scope.matches.splice(index, 1);
             };
         }]
     };
-}]);  
+}]);
